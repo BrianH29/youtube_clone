a492131e93609d71a50ad17157355729c289e4a0
Brian Hwang <brianhwang29@gmail.com>
2022-02-01 21:46:06 +0900

    hashtag component

---
+++ src/components/HashTag.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="hashtag">
+    <ul>
+      <li>#nbafinal</li>
+      <li>#highlight</li>
+      <li>#warriorsvsrockets</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.hashtag {
+  background: map-get($font-color, 'black');
+  padding: map-get($font-space, 'spacing');
+
+  ul {
+    display: flex;
+    list-style: none;
+    color: map-get($font-color, 'hashBlue');
+    font-size: map-get($font-size, 'small');
+
+    li {
+      margin-right: map-get($font-space, 'spacing');
+    }
+  }
+}
+</style>
